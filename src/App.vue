06929c6cec08e536704469c68c82dafa2d850617
lemony-cake <papaya.dev@protonmail.com>
2024-02-24 14:18:51 +1300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p class="typography-display-2xl">Display 2xl</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import './foundations/typography.less'
+import App from './App.vue'
+
+createApp(App).mount('#app')
